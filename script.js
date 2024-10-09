526e8b6e67d3e49dcf3eca03fd5852b92a4139f8
vishal0714 <122671523+vishal0714@users.noreply.github.com>
2024-10-09 14:31:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function firstChar(text) {
-  // your code here
+    // Iterate through each character in the string
+    for (let char of text) {
+        // Return the first non-space character
+        if (char !== ' ') {
+            return char;
+        }
+    }
+    // If no non-space character is found, return an empty string
+    return '';
 }
 
+// Test cases
+console.log(firstChar(' Rosa Parks '));  // Output: 'R'
+console.log(firstChar(' Hello World '));  // Output: 'H'
+console.log(firstChar(' '));  // Output: ''
+console.log(firstChar(''));  // Output: ''
+
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+// Uncomment the following line to show the prompt popup
+// const text = prompt("Enter text:");
 alert(firstChar(text));
